0f1223e01024c042fea6eff3603f7a155ea3b5f3
DrPsychoX <kallinos.loizos@gmail.com>
2022-03-09 12:35:16 +0200

    AssetPush-Check

---
+++ Assets/UI_Manager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+    [Header("Panels")] 
+    [SerializeField] private Transform mainPanel;
+    [SerializeField] private Transform optionPanel;
+    [SerializeField] private Transform creditsPanel;
+    
+    [Header("Buttons")]
+    [SerializeField] private Button playBtn;
+    [SerializeField] private Button optionsBtn;
+    [SerializeField] private Button creditsBtn;
+    [SerializeField] private Button quitBtn;
+    
+    void Start()
+    {
+        playBtn.onClick.AddListener(GoToGameScene);
+        optionsBtn.onClick.AddListener(GoToOption);
+        creditsBtn.onClick.AddListener(GoToCredits);
+        quitBtn.onClick.AddListener(QuitGame);
+    }
+    
+    void GoToGameScene()
+    {
+        SceneManager.LoadSceneAsync(sceneBuildIndex: 1);
+    }
+
+    void GoToOption()
+    { 
+        optionPanel.gameObject.SetActive(true);
+    }
+
+    void GoToCredits()
+    {
+        creditsPanel.gameObject.SetActive(true);
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+}
