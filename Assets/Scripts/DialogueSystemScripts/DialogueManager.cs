988a1739b139f2b02eab624c7efc8a8a1fd77863
AlexandrosNeofytou <101253791+AlexandrosNeofytou@users.noreply.github.com>
2022-03-17 22:18:58 +0200

    Rebased with correct git files

---
+++ Assets/Scripts/DialogueSystemScripts/DialogueManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] Text dialogueText;
+
+    Queue<string> dialogueLines = new Queue<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartDilaogue(Dialogue currentDialogue)
+    {
+        foreach(string dialogue in currentDialogue.Dialogues())
+        {
+            dialogueLines.Enqueue(dialogue);
+        }
+    }
+}
